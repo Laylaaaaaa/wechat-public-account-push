aeaeef0617e2a8013d442e28b0fc3abeafc2ff4c
The Real Laya <44071133+Laylaaaaaa@users.noreply.github.com>
2022-10-16 09:02:54 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,37 +1,58 @@
-/* eslint-disable */
-
-/**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
 const USER_CONFIG = {
 
-  // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx742245ff1cab6597',
-
-  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '76328c7ba7dd76caff200827873ef0f9',
 
-  PROVINCE: '辽宁',
-  CITY: '沈阳',
+  // PROVINCE: '辽宁',
+  // CITY: '沈阳',
 
   USERS: [
+    // {
+    //   name: '哥哥',
+    //   id: 'oelhR5-vJRhT6_3v2zuJkZYzHN8U',
+    //   useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
+    //   horoscopeDate: '05-13',
+    //   festivals: [
+    //     {
+    //       type: '生日', name: '哥哥', year: '1992', date: '05-13',
+    //     },
+    //     {
+    //       type: '生日', name: '宝宝', year: '1995', date: '12-30',
+    //     },
+    //   ],
+    //   customizedDateList: [
+    //     { keyword: 'love_day', date: '2019-12-08' },
+    //   ],
+    // },
+    
     {
-      // 想要发送的人的名字
+      name: '妈妈',
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      useTemplateId: '75BaCz8MGol2WS4qAIEM__9r_xkdx6hXVPfPk37sVPw',
+      horoscopeDate: '05-27',
+      city: '长春',
+    },
+
+    {
+      name: '爸爸',
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      useTemplateId: 'yasFbW5ZV9-EGIhKdK9lCajExekzcmxPZVQYCo1nsSc',
+      horoscopeDate: '05-21',
+      city: '长春',
+    },
+
+
+
+
+
+
+
+    {
       name: '哥哥',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oelhR5-vJRhT6_3v2zuJkZYzHN8U',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
       useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-13',
       festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {
-//           type: '*生日', name: '哥哥', year: '1992', date: '05-13',
-//         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '哥哥', year: '1992', date: '05-13',
         },
@@ -39,56 +60,18 @@ const USER_CONFIG = {
           type: '生日', name: '宝宝', year: '1995', date: '12-30',
         },
       ],
-      // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
-        // 在一起的日子
         { keyword: 'love_day', date: '2019-12-08' },
-        // 结婚纪念日
-//         { keyword: 'marry_day', date: '2019-12-08' },
       ],
     },
     
-        {
-      // 想要发送的人的名字
-      name: '哥哥',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-13',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-//         {
-//           type: '*生日', name: '哥哥', year: '1992', date: '05-13',
-//         },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '哥哥', year: '1992', date: '05-13',
-        },
-        {
-          type: '生日', name: '宝宝', year: '1995', date: '12-30',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2019-12-08' },
-        // 结婚纪念日
-//         { keyword: 'marry_day', date: '2019-12-08' },
-      ],
-    }
-    
   ],
 
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'i6zdJgzCizfbLOLFmFl2iHmLQTvmvkYcm1iiLb26ok8',
 
   CALLBACK_USERS: [
     {
       name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
     }
   ],
