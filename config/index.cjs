cf03bef2288626702821d399bc93091d17804b4c
The Real Laya <44071133+Laylaaaaaa@users.noreply.github.com>
2022-10-16 08:30:32 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wwcTwvXIE-n1t_ePEX0ZRGEwZs1tfFYxi_EV47yOeQA',
+      useTemplateId: 'JGJ3mWJgd7H4sZbmcrdVaB_7gGVu39rslsGzI_82zm0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-13',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'CZ2xz3mIxNt62KIzjWhn17Bq30Vv3XfgpXQhq1nstG8',
+  CALLBACK_TEMPLATE_ID: 'i6zdJgzCizfbLOLFmFl2iHmLQTvmvkYcm1iiLb26ok8',
 
   CALLBACK_USERS: [
     {
