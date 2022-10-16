5351fcf091d6ec74027bec788d79afd746c1e450
The Real Laya <44071133+Laylaaaaaa@users.noreply.github.com>
2022-10-16 09:09:57 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -3,55 +3,13 @@ const USER_CONFIG = {
   APP_ID: 'wx742245ff1cab6597',
   APP_SECRET: '76328c7ba7dd76caff200827873ef0f9',
 
-  // PROVINCE: '辽宁',
-  // CITY: '沈阳',
-
   USERS: [
-    // {
-    //   name: '哥哥',
-    //   id: 'oelhR5-vJRhT6_3v2zuJkZYzHN8U',
-    //   useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
-    //   horoscopeDate: '05-13',
-    //   festivals: [
-    //     {
-    //       type: '生日', name: '哥哥', year: '1992', date: '05-13',
-    //     },
-    //     {
-    //       type: '生日', name: '宝宝', year: '1995', date: '12-30',
-    //     },
-    //   ],
-    //   customizedDateList: [
-    //     { keyword: 'love_day', date: '2019-12-08' },
-    //   ],
-    // },
-    
-    {
-      name: '妈妈',
-      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
-      useTemplateId: '75BaCz8MGol2WS4qAIEM__9r_xkdx6hXVPfPk37sVPw',
-      horoscopeDate: '05-27',
-      city: '长春',
-    },
-
-    {
-      name: '爸爸',
-      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
-      useTemplateId: 'yasFbW5ZV9-EGIhKdK9lCajExekzcmxPZVQYCo1nsSc',
-      horoscopeDate: '05-21',
-      city: '长春',
-    },
-
-
-
-
-
-
-
     {
       name: '哥哥',
-      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      id: 'oelhR5-vJRhT6_3v2zuJkZYzHN8U',
       useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
       horoscopeDate: '05-13',
+      city: '沈阳',
       festivals: [
         {
           type: '生日', name: '哥哥', year: '1992', date: '05-13',
@@ -63,6 +21,57 @@ const USER_CONFIG = {
       customizedDateList: [
         { keyword: 'love_day', date: '2019-12-08' },
       ],
+    },
+    
+    // {
+    //   name: '妈妈',
+    //   id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+    //   useTemplateId: '75BaCz8MGol2WS4qAIEM__9r_xkdx6hXVPfPk37sVPw',
+    //   horoscopeDate: '05-27',
+    //   city: '长春',
+    // },
+
+    // {
+    //   name: '爸爸',
+    //   id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+    //   useTemplateId: 'yasFbW5ZV9-EGIhKdK9lCajExekzcmxPZVQYCo1nsSc',
+    //   horoscopeDate: '05-21',
+    //   city: '长春',
+    // },
+
+    {
+      name: '哥哥test',
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
+      horoscopeDate: '05-13',
+      city: '沈阳',
+      festivals: [
+        {
+          type: '生日', name: '哥哥', year: '1992', date: '05-13',
+        },
+        {
+          type: '生日', name: '宝宝', year: '1995', date: '12-30',
+        },
+      ],
+      customizedDateList: [
+        { keyword: 'love_day', date: '2019-12-08' },
+      ],
+    },
+
+    {
+      name: '妈妈test',
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      useTemplateId: '75BaCz8MGol2WS4qAIEM__9r_xkdx6hXVPfPk37sVPw',
+      horoscopeDate: '05-27',
+      city: '长春',
+    },
+
+    {
+      name: '爸爸test',
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      useTemplateId: 'yasFbW5ZV9-EGIhKdK9lCajExekzcmxPZVQYCo1nsSc',
+      horoscopeDate: '05-21',
+      city: '长春',
     },
     
   ],
