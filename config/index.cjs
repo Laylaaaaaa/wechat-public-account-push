2e798b018e9444302ad1ddd0068af9acd663b549
The Real Laya <44071133+Laylaaaaaa@users.noreply.github.com>
2022-10-16 08:47:06 -0700

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '哥哥',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      id: 'oelhR5-vJRhT6_3v2zuJkZYzHN8U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '_vvBtl1i61cSOOXdSa6FUwChr5qVxHIze4pIYN35HTs',
+      useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-13',
       festivals: [
@@ -47,6 +47,38 @@ const USER_CONFIG = {
 //         { keyword: 'marry_day', date: '2019-12-08' },
       ],
     },
+    
+        {
+      // 想要发送的人的名字
+      name: '哥哥',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oelhR5x5lAmEEnKHl_qqe83nzAFs',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'W9mERC4cen0jhE4FwbIh_Dzo-KL1E_-WKhwWn1dw7b8',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '05-13',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: '哥哥', year: '1992', date: '05-13',
+//         },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '哥哥', year: '1992', date: '05-13',
+        },
+        {
+          type: '生日', name: '宝宝', year: '1995', date: '12-30',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2019-12-08' },
+        // 结婚纪念日
+//         { keyword: 'marry_day', date: '2019-12-08' },
+      ],
+    }
+    
   ],
 
 
